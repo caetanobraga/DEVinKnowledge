ee0e31cdc1baf36af0a274c6690164065e7a0878
Caetano Braga <caetanobraga@gmail.com>
2022-08-07 17:43:47 -0300

    commit final projeto DEVinKnowledge

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const linguagemSkill = document.getElementById("linguagemSkill")
 const categoria = document.getElementById("categoria");
 const descricao = document.getElementById("descricao");
 const urlYouTube = document.getElementById("urlYouTube");
+const tituloInvalido = document.getElementById("tituloInvalido");
+const linguagemInvalida = document.getElementById("linguagemInvalida");
+const categoriaInvalida = document.getElementById("categoriaInvalida");
+const descricaoInvalida = document.getElementById("descricaoInvalida");
+const urlInvalida = document.getElementById("urlInvalida");
 const botaoLimpaForm = document.getElementById("limpaForm");
 const formDica = document.getElementById("formDica");
 const listaDeCards = document.getElementById("listaDeCards");
@@ -25,6 +30,11 @@ const limpaForm = () => {
     categoria.value="0";
     descricao.value="";
     urlYouTube.value="";
+    tituloInvalido.innerHTML="";
+    linguagemInvalida.innerHTML="";
+    categoriaInvalida.innerHTML="";
+    descricaoInvalida.innerHTML="";
+    urlInvalida.innerHTML="";
 }
 
 const criaCard = (card) =>{
@@ -54,7 +64,7 @@ const criaCard = (card) =>{
 
     botaoYoutube.classList.add("cardButton");
     botaoYoutube.innerHTML = ("<img src='./images/video.svg'></img>");
-    botaoYoutube.onclick = () => abreVideo(card.id);
+    botaoYoutube.onclick = () => abreVideo(card.video);
 
     divTitulo.innerHTML = (`<h2>${card.titulo}</h2>`);
     divDadosCard.innerHTML = (`<span><strong>Linguagem/Skill:</strong>${card.linguagem}</span>
@@ -84,22 +94,40 @@ const atualizaTotais = () =>{
     totalDicasSoft.innerHTML = lStorage.filter((item)=> item.categoria === "4").length;
 }
 
-let lStorage =[];
-lStorage = JSON.parse(localStorage.getItem("listaDeDicas"))
-lStorage.forEach(card => {
-    criaCard (card);
-    
-});
+
+let lStorage = JSON.parse(localStorage.getItem("listaDeDicas"))
+if (!lStorage)
+    lStorage = [];
+    lStorage.forEach(card => {
+        criaCard (card);
+    });
 atualizaTotais();
-
-
 
 formDica.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log('oi');
     if (indiceEdicao.value){
-        console.log(indiceEdicao.value)
-        console.log(lStorage);
+        if (event.target.titulo.value.length < 8 || event.target.titulo.value.length >64){
+            tituloInvalido.innerHTML = "Título deve ter entre 8 e 64 caracteres"
+            return
+        }
+        if (event.target.linguagemSkill.value.length < 4 || event.target.linguagemSkill.value.length > 16){
+            linguagemInvalida.innerHTML = "Skill deve ter entre 4 e 16 caracteres"
+            return
+        }
+        if (event.target.categoria.value == "0"){
+            categoriaInvalida.innerHTML = "Selecione uma categoria!"
+            return
+        }
+        if (event.target.descricao.value.length < 32 || event.target.descricao.value.length > 512){
+            descricaoInvalida.innerHTML = "Descrição deve ter entre 32 e 512 caracteres"
+            return
+        }
+        if (urlYouTube.value.length > 0){
+            if (!isYoutubeVideo(urlYouTube.value)){
+                urlInvalida.innerHTML = "Endereço de vídeo inválido!"
+                return
+            }
+        }
         const card = new Dica(titulo.value, linguagemSkill.value, categoria.value, descricao.value, urlYouTube.value);
         lStorage.splice(indiceEdicao.value,1,card)
         localStorage.setItem("listaDeDicas",JSON.stringify(lStorage));
@@ -108,28 +136,57 @@ formDica.addEventListener("submit", (event) => {
             criaCard (card);
         });
         atualizaTotais();
+        alert("Dica alterada com sucesso!");
         return;
     };
-        
-    console.log('oiiiii');
+    if (event.target.titulo.value.length < 8 || event.target.titulo.value.length >64){
+        tituloInvalido.innerHTML = "Título deve ter entre 8 e 64 caracteres"
+        return
+    }
+    if (event.target.linguagemSkill.value.length < 4 || event.target.linguagemSkill.value.length > 16){
+        linguagemInvalida.innerHTML = "Skill deve ter entre 4 e 16 caracteres"
+        return
+    }
+    if (event.target.categoria.value == "0"){
+        categoriaInvalida.innerHTML = "Selecione uma categoria!"
+        return
+    }
+    if (event.target.descricao.value.length < 32 || event.target.descricao.value.length > 512){
+        descricaoInvalida.innerHTML = "Descrição deve ter entre 32 e 512 caracteres"
+        return
+    }
+    if (urlYouTube.value.length > 0){
+        if (!isYoutubeVideo(urlYouTube.value)){
+            urlInvalida.innerHTML = "Endereço de vídeo inválido!"
+            return
+        }
+    }
     const card = new Dica(titulo.value, linguagemSkill.value, categoria.value, descricao.value, urlYouTube.value);
     lStorage.push(card);
     criaCard(card);
-    console.log(card);
     localStorage.setItem("listaDeDicas",JSON.stringify(lStorage));
     atualizaTotais();
-    
-
-
+    alert("Dica cadastrada com sucesso!");
 });
+
+botaoPesquisaDica.addEventListener("click", (event) => {
+    event.preventDefault()
+    pesquisaDica(campoPesquisa.value);
+})
 
 botaoLimpaForm.addEventListener("click", (event) => {
     event.preventDefault()
     limpaForm();
 });
 
+function isYoutubeVideo(url) {
+    var v = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    return (url.match(v)) ? RegExp.$1 : false;
+}
+
 function deleta(id){
-    lStorage = JSON.parse(localStorage.getItem("listaDeDicas"));
+    if (!window.confirm("Deseja realmente deletar a dica?"))
+    return
     let indice = lStorage.findIndex(dica => dica.id === id);
     lStorage.splice(indice,1);    
     localStorage.setItem("listaDeDicas",JSON.stringify(lStorage));
@@ -141,48 +198,48 @@ function deleta(id){
 }
 
 function edita(id){
-    lStorage = JSON.parse(localStorage.getItem("listaDeDicas"));
     let indice = lStorage.findIndex(dica => dica.id === id);
-    console.log(indice);
     indiceEdicao.value = indice;
-    console.log(indiceEdicao.value);
     titulo.value = lStorage[indice].titulo;
     linguagemSkill.value = lStorage[indice].linguagem;
     categoria.value = lStorage[indice].categoria;
     descricao.value = lStorage[indice].descricao;
     urlYouTube.value = lStorage[indice].video;
+    alert("Os dados para edição foram carregados no formulário lateral, realize as alterações e clique em salvar para concluir!")
 }
 
-function abreVideo(id){
-    console.log("abrindo video",id);
+function abreVideo(urlVideo){
+    window.open(urlVideo, '_blank');
+    console.log("abrindo video", urlVideo);
 }
-
-botaoPesquisaDica.addEventListener("click", (event) => {
-    event.preventDefault()
-    pesquisaDica(campoPesquisa.value);
-})
     
-function pesquisaDica(campoPesquisa){
-    let pesquisa = lStorage.filter((item)=> item.titulo === campoPesquisa)
-    if (pesquisa){
+function pesquisaDica(string){
+    let pesquisa = [];
+    if(!string){
+        alert('digite para pesquisar')
+        return;
+    }
+    lStorage.forEach((card) =>{
+        if (card.titulo.includes(string)){
+            pesquisa.push(card);
+        }
+    })
+    console.log(pesquisa)
+    if (pesquisa.length === 0){
+        alert("Título não localizado");
+        campoPesquisa.value="";
+        return;
+    }
     listaDeCards.innerHTML="";
     pesquisa.forEach(card => {
         criaCard (card);   
     });
-    console.log(pesquisa);
-}
 }
 
 botaoLimpaPesquisa.addEventListener("click", (event) =>{
     campoPesquisa.value = "";
     listaDeCards.innerHTML="";
     lStorage.forEach(card => {
-        criaCard (card);
-        
+        criaCard (card);       
     });
 })
-
-
-
-
-//lStorage = JSON.parse(localStorage.getItem("listaDeDicas"));
